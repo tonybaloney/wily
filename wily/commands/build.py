96ef3e6824f06a193d1cbeda4737c6359304cb8c
Anthony Shaw <anthonyshaw@apache.org>
2018-10-14 09:57:58 +1100

    use the cache module in the build process

--- wily/commands/build.py
+++ wily/commands/build.py
@@ -1,8 +1,8 @@
 """
 Builds a cache based on a source-control history
 """
+import wily.cache as cache
 import logging
-import warnings
 
 logger = logging.getLogger("wily")
 
@@ -20,11 +20,20 @@ def build(config, archiver, operators):
     :param operators: The list of operators to execute
     :type operators: `list` of :namedtuple:`wily.operators.Operator`
     """
+    # Check for existence of cache, else provision
+    if not cache.exists():
+        logging.debug("Wily cache not found, creating.")
+        cache.create()
+        logging.debug("Created wily cache")
+
+    logging.debug(f"Using {archiver.name} archiver module")
     archiver = archiver.cls(config)
+
     revisions = archiver.revisions(config.path, config.max_revisions)
+    logging.info(f"Found {len(revisions)} revisions")
 
     if revisions is None or len(revisions) == 0:
-        logger.warn("Could not find any revisions, using HEAD")
+        logger.warning("Could not find any revisions, using HEAD")
         revisions = []  # TODO: Create a special HEAD revision to use current state
     
     # Build a set of operators
@@ -35,5 +44,6 @@ def build(config, archiver, operators):
         # TODO: Verify there aren't any non-committed files in working copy
 
         for operator in operators:
-            operator.run(revision, config)
-
+            logging.debug(f"Running {operator} on {revision}")
+            stats = operator.run(revision, config)
+            cache.store(archiver, revision, stats)
