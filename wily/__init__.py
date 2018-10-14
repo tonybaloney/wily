051b597f4f3b09af1472af60e61146588b076dc2
Anthony Shaw <anthonyshaw@apache.org>
2018-10-14 14:52:50 +1100

    add wily entry point

--- wily/__init__.py
+++ wily/__init__.py
@@ -3,7 +3,7 @@ A Python application for tracking, reporting on timing and complexity in tests a
 """
 import colorlog
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 _handler = colorlog.StreamHandler()
 _handler.setFormatter(colorlog.ColoredFormatter("%(log_color)s%(message)s"))
