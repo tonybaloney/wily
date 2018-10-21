4e6732845f5d481f3b2fc095cb64092fc3aba40c
Anthony Shaw <tonybaloney@users.noreply.github.com>
2018-10-21 08:56:19 +0100

    Docstrings, formatting and todo items for the config module

--- wily/config.py
+++ wily/config.py
@@ -1,8 +1,10 @@
 """
 Configuration of wily
 
-TODO : Handle operator settings
+TODO : Handle operator settings. Maybe a section for each operator and then pass kwargs to operators?
 TODO : Allow configuration of cache path (incase it needs to go in another folder)
+TODO : Better utilise default values and factory in @dataclass to replace DEFAULT_CONFIG
+ and replace the logic in load() to set default values.
 """
 
 import configparser
@@ -18,7 +20,12 @@ logger = logging.getLogger(__name__)
 
 
 @dataclass
-class WilyConfig:
+class WilyConfig(object):
+    """
+    A data class to reflect the configurable options
+    within Wily.
+    """
+
     operators: List
     archiver: Any
     path: str
@@ -27,26 +34,36 @@ class WilyConfig:
     checkout_options: dict = field(default_factory=dict)
 
     def __post_init__(self):
+        # Clone targets as a list of path
         if self.targets is None or "":
             self.targets = [self.path]
 
+# Default values for Wily
 
+""" The default operators """
 DEFAULT_OPERATORS = {
     operators.OPERATOR_RAW.name,
     operators.OPERATOR_MAINTAINABILITY.name,
     operators.OPERATOR_CYCLOMATIC.name,
 }
 
+""" The name of the default archiver """
 DEFAULT_ARCHIVER = ARCHIVER_GIT.name
 
+""" The default configuration file name """
 DEFAULT_CONFIG_PATH = "wily.cfg"
 
+""" The default section name in the config """
 DEFAULT_CONFIG_SECTION = "wily"
 
+""" The default path name to the cache """
 DEFAULT_CACHE_PATH = ".wily"
 
+""" The default maximum number of revisions to archiver """
 DEFAULT_MAX_REVISIONS = 100
 
+
+""" The default configuration for Wily (if no config file exists) """
 DEFAULT_CONFIG = WilyConfig(
     operators=DEFAULT_OPERATORS,
     archiver=DEFAULT_ARCHIVER,
@@ -54,15 +71,18 @@ DEFAULT_CONFIG = WilyConfig(
     max_revisions=DEFAULT_MAX_REVISIONS,
 )
 
+""" Default table style in console. See tabulate docs for more. """
 DEFAULT_GRID_STYLE = "fancy_grid"
 
 
 def load(config_path=DEFAULT_CONFIG_PATH):
     """
-    Load config file
+    Load config file and set values to defaults where no present.
 
     :return: The configuration ``WilyConfig``
+    :rtype: :class:`wily.config.WilyConfig`
     """
+
     if not pathlib.Path(config_path).exists():
         logger.debug(f"Could not locate {config_path}, using default config.")
         return DEFAULT_CONFIG
@@ -76,5 +96,8 @@ def load(config_path=DEFAULT_CONFIG_PATH):
     max_revisions = config.get(option="max_revisions", fallback=DEFAULT_MAX_REVISIONS)
 
     return WilyConfig(
-        operators=operators, archiver=archiver, path=path, max_revisions=max_revisions
+        operators=operators, 
+        archiver=archiver, 
+        path=path, 
+        max_revisions=max_revisions
     )
