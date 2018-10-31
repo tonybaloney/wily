66116b14ca39b33a6414d6965859366a8e3ec8fb
Anthony Shaw <anthonyshaw@apache.org>
2018-10-31 13:20:49 +1100

    add some basic tests for the cli

--- test/test_cli.py
+++ test/test_cli.py
@@ -1,2 +1,37 @@
 import wily.__main__ as main
+from mock import patch
+from click.testing import CliRunner
 
+
+def test_help():
+    """
+    Test that CLI when called with help options
+    """
+    with patch('wily.logger') as logger:
+        runner = CliRunner()
+        result = runner.invoke(main.cli, ['--help', '--debug'])
+        assert result.exit_code == 0
+
+
+def test_build():
+    """
+    Test that build calls the build command
+    """
+    with patch('wily.commands.build.build') as build:
+        runner = CliRunner()
+        result = runner.invoke(main.cli, ['build'])
+        assert result.exit_code == 0
+        assert build.called_once
+
+
+def test_build_with_opts():
+    """
+    Test that build calls the build command
+    """
+    with patch('wily.commands.build.build') as build:
+        runner = CliRunner()
+        result = runner.invoke(main.cli, ['build', '-h 1', '-o raw,maintainability'])
+        assert result.exit_code == 0
+        assert build.called_once
+        assert build.call_args[1]['config'].max_revisions == 1
+        assert build.call_args[1]['config'].operators == ['raw', 'maintainability']
