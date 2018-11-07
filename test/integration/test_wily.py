922eeab9a1d154c550f5d7061a018f0d220740ad
Anthony Shaw <anthony.p.shaw@gmail.com>
2018-11-07 16:21:32 +1100

    create integration test as a fixture

--- test/integration/test_wily.py
+++ test/integration/test_wily.py
@@ -3,6 +3,33 @@ from mock import patch
 from click.testing import CliRunner
 from git import Repo, Actor
 import pathlib
+import pytest
+
+
+@pytest.fixture
+def builddir(tmpdir):
+    """ Create a project and build it """
+    repo = Repo.init(path=tmpdir)
+    tmppath = pathlib.Path(tmpdir)
+
+    # Write a test file to the repo
+    with open(tmppath / "test.py", "w") as test_txt:
+        test_txt.write("import abc")
+
+    index = repo.index
+    index.add(["test.py"])
+
+    author = Actor("An author", "author@example.com")
+    committer = Actor("A committer", "committer@example.com")
+
+    index.commit("basic test", author=author, committer=committer)
+
+    with patch("wily.logger"):
+        runner = CliRunner()
+        result = runner.invoke(main.cli, ["--debug", "--path", tmpdir, "build"])
+        assert result.exit_code == 0
+
+    return tmpdir
 
 
 def test_build_not_git_repo(tmpdir):
@@ -51,3 +78,13 @@ def test_build(tmpdir):
 
     cache_path = tmpdir / ".wily"
     assert cache_path.exists()
+
+
+def test_report(builddir):
+    """
+    Test that report works with a build
+    """
+    with patch("wily.logger") as logger:
+        runner = CliRunner()
+        result = runner.invoke(main.cli, ["--path", builddir, "report"])
+        assert result.exit_code == 0
