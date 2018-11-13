d90206b80be6090f569809dde00e21c6eed0dd37
Anthony Shaw <anthonyshaw@apache.org>
2018-11-14 10:35:57 +1100

    more tests for multiple metrics

--- test/integration/test_wily.py
+++ test/integration/test_wily.py
@@ -291,6 +291,16 @@ def test_index(builddir):
         assert result.exit_code == 0, result.stdout
 
 
+def test_index_with_messages(builddir):
+    """
+    Test that index works with a build with git commit messages
+    """
+    with patch("wily.logger") as logger:
+        runner = CliRunner()
+        result = runner.invoke(main.cli, ["--path", builddir, "index", "--message"])
+        assert result.exit_code == 0, result.stdout
+
+
 def test_list_metrics(builddir):
     """
     Test that list-metrics works with a build
@@ -317,6 +327,16 @@ def test_graph(builddir):
         runner = CliRunner()
         result = runner.invoke(
             main.cli, ["--path", builddir, "graph", "test.py", "raw.loc"]
+        )
+        assert result.exit_code == 0, result.stdout
+
+
+def test_graph_multiple(builddir):
+    """ Test the graph feature with multiple metrics """
+    with patch("wily.logger") as logger:
+        runner = CliRunner()
+        result = runner.invoke(
+            main.cli, ["--path", builddir, "graph", "test.py", "raw.loc", "raw.comments"]
         )
         assert result.exit_code == 0, result.stdout
 
