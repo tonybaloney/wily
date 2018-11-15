19ee533bc89a81f3b86589f608b00fec8fc7704a
Anthony Shaw <anthonyshaw@apache.org>
2018-11-15 16:46:00 +1100

    update rev

--- wily/__init__.py
+++ wily/__init__.py
@@ -5,7 +5,7 @@ import colorlog
 import datetime
 
 
-__version__ = "1.4.0.b1"
+__version__ = "1.4.0"
 
 _handler = colorlog.StreamHandler()
 _handler.setFormatter(colorlog.ColoredFormatter("%(log_color)s%(message)s"))
