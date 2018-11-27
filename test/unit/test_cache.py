c216830fb5f90d3dad956e2e18f86bf45baa6845
Anthony Shaw <anthony.p.shaw@gmail.com>
2018-11-27 19:04:05 +1100

    add a test for index versioning

--- test/unit/test_cache.py
+++ test/unit/test_cache.py
@@ -16,6 +16,19 @@ def test_exists(tmpdir):
     assert cache.exists(config)
 
 
+def test_exists_older(tmp_path):
+    """
+    Test that exists() returns true if path does exist
+    and has a file with the version older than the current
+    """
+    config = DEFAULT_CONFIG
+    config.cache_path = tmp_path
+    (tmp_path / ".wily").mkdir()
+    with open((tmp_path / ".wily" / "index.json"), "w+") as index:
+        index.write("{'version': '0.1.0'}")
+    assert cache.exists(config)
+
+
 def test_not_exists():
     """
     Test that exists() returns false if path does not exist
