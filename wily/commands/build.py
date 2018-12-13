d81d4be709a49d0f36c0dd9efb4a545dc630faa5
Anthony Shaw <anthonyshaw@apache.org>
2018-12-13 23:59:24 +0800

    match on the string version of the path

--- wily/commands/build.py
+++ wily/commands/build.py
@@ -95,7 +95,7 @@ def build(config, archiver, operators):
                     
                     for root in roots:
                         # find all matching entries recursively
-                        aggregates = [path for path in result.keys() if pathlib.Path(path).match(root + "/**/*")]
+                        aggregates = [path for path in result.keys() if pathlib.Path(path).match(str(root) + "/**/*")]
                     
                         # aggregate values
                         for metric in resolve_operator(operator_name).cls.metrics:
