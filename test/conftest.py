ec1c0b318c32387fb14b0a754fb576154e7056b1
Anthony Shaw <anthonyshaw@apache.org>
2019-01-19 11:16:44 +1100

    update test names based on docstrings

--- test/conftest.py
+++ test/conftest.py
@@ -94,3 +94,18 @@ def cache_path(monkeypatch):
     monkeypatch.setenv("HOME", tmp)
     yield tmp
     shutil.rmtree(tmp)
+
+
+def pytest_itemcollected(item):
+    parent = item.parent.obj  # Test class/module
+    node = item.obj  # Test case
+    suite_doc = parent.__doc__.strip() if parent.__doc__ else None
+    case_doc = node.__doc__.strip() if node.__doc__ else None
+    if suite_doc and case_doc:
+        item._nodeid = '{0} [{1}]'.format(suite_doc, case_doc)
+    elif suite_doc and not case_doc:
+        item._nodeid = '{0} [{1}]'.format(suite_doc, node.__name__)
+    elif case_doc and not suite_doc:
+        item._nodeid = '{0} [{1}]'.format(parent.__name__, case_doc)
+    else:
+        item._nodeid = node.__name__
