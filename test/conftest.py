b5b3b4b2d54f4d7c4f506b9ca78898e17df196e8
Anthony Shaw <anthonyshaw@apache.org>
2019-11-29 18:01:06 +0800

    Tests for the ipynb features

--- test/conftest.py
+++ test/conftest.py
@@ -1,6 +1,5 @@
 import pathlib
 from textwrap import dedent
-import os
 import shutil
 import tempfile
 import pytest
@@ -83,6 +82,157 @@ def builddir(gitdir):
     assert result2.exit_code == 0, result2.stdout
 
     return gitdir
+
+
+@pytest.fixture
+def ipynbgitdir(tmpdir):
+    """
+    A fixture that provides a directory and a working Git DB,
+    contains a single IPython notebook that has 3 revisions.
+    """
+    _NB_FOOTER = """
+     "metadata": {
+          "kernelspec": {
+           "display_name": "Python 3",
+           "language": "python",
+           "name": "python3"
+          },
+          "language_info": {
+           "codemirror_mode": {
+            "name": "ipython",
+            "version": 3
+           },
+           "file_extension": ".py",
+           "mimetype": "text/x-python",
+           "name": "python",
+           "nbconvert_exporter": "python",
+           "pygments_lexer": "ipython3",
+           "version": "3.4.2"
+          }
+         },
+     "nbformat": 4,
+     "nbformat_minor": 0
+    """
+    repo = Repo.init(path=tmpdir)
+    tmppath = pathlib.Path(tmpdir)
+    testpath = tmppath / "src" / "test.ipynb"
+    (tmppath / "src").mkdir()
+    # Write a test file to the repo
+    with open(testpath, "w") as test_txt:
+        test_txt.write("{\"cells\": []," + _NB_FOOTER + "}")
+
+    index = repo.index
+    index.add([str(testpath)])
+
+    author = Actor("An author", "author@example.com")
+    committer = Actor("A committer", "committer@example.com")
+
+    index.commit("empty notebook", author=author, committer=committer)
+
+    first_test = """{
+     "cells": [
+      {
+       "cell_type": "code",
+       "metadata": {},
+       "language": "python",
+       "source": [
+        "import abc\\n",
+        "foo = 1\\n",
+        "def function1():\\n",
+        "    a = 1 + 1\\n",
+        "\\n",
+        "class Class1(object):\\n",
+        "    def method(self):\\n",
+        "        b = 1 + 5\\n"
+       ],
+       "outputs": [],
+       "execution_count": 0,
+       "input": []
+      }
+      ],
+    """ + _NB_FOOTER + "}"
+    with open(testpath, "w") as test_txt:
+        test_txt.write(dedent(first_test))
+
+    index.add([str(testpath)])
+    index.commit("single cell", author=author, committer=committer)
+
+    second_test = """{
+     "cells": [
+      {
+       "cell_type": "code",
+       "metadata": {},
+       "language": "python",
+       "source": [
+        "import abc\\n",
+        "foo = 1\\n",
+        "def function1():\\n",
+        "    a = 1 + 1\\n",
+        "class Class1(object):\\n",
+        "    def method(self):\\n",
+        "        b = 1 + 5\\n",
+        "        if b == 6:\\n",
+        "            return 'banana'\\n"
+       ],
+       "outputs": [],
+       "execution_count": 0,
+       "input": []
+      },
+      {
+       "cell_type": "code",
+       "metadata": {},
+       "language": "python",
+       "source": [
+        "foo = 1\\n",
+        "class Class1(object):\\n",
+        "    def method(self):\\n",
+        "        b = 1 + 5\\n",
+        "        if b == 6:\\n",
+        "            return 'banana'\\n"
+       ],
+       "outputs": [],
+       "execution_count": 0,
+       "input": []
+      }
+      ],
+    """ + _NB_FOOTER + "}"
+
+    with open(testpath, "w") as test_txt:
+        test_txt.write(dedent(second_test))
+
+    index.add([str(testpath)])
+    index.commit("second cell", author=author, committer=committer)
+
+    yield tmpdir
+    repo.close()
+
+
+@pytest.fixture
+def ipynbbuilddir(ipynbgitdir):
+    """
+    The ipynbgitdir fixture converted into a wily cache index.
+    """
+    tmppath = pathlib.Path(ipynbgitdir)
+
+    config = """
+    [wily]
+    include_ipynb = true
+    ipynb_cells = true
+    """
+    config_path = tmppath / "wily.cfg"
+    with open(config_path, "w") as config_f:
+        config_f.write(config)
+
+    runner = CliRunner()
+    result1 = runner.invoke(
+        main.cli, ["--debug", "--path", ipynbgitdir, "build", str(tmppath / "src")]
+    )
+    assert result1.exit_code == 0, result1.stdout
+
+    result2 = runner.invoke(main.cli, ["--debug", "--path", ipynbgitdir, "index"])
+    assert result2.exit_code == 0, result2.stdout
+
+    return ipynbgitdir
 
 
 @pytest.fixture(autouse=True)
--- test/integration/test_index.py
+++ test/integration/test_index.py
@@ -4,8 +4,12 @@ import wily.__main__ as main
 
 
 def test_index_no_cache(tmpdir, cache_path):
+    """
+    Test that wily index fails in a directory that has no cache
+    """
     runner = CliRunner()
     result = runner.invoke(main.cli, ["--path", tmpdir, "--cache", cache_path, "index"])
+    assert "An author" not in result.stdout
     assert result.exit_code == 1, result.stdout
 
 
@@ -15,6 +19,7 @@ def test_index(builddir):
     """
     runner = CliRunner()
     result = runner.invoke(main.cli, ["--path", builddir, "index"])
+    assert result.stdout.count("An author") == 3
     assert result.exit_code == 0, result.stdout
 
 
@@ -24,4 +29,8 @@ def test_index_with_messages(builddir):
     """
     runner = CliRunner()
     result = runner.invoke(main.cli, ["--path", builddir, "index", "--message"])
+    assert result.stdout.count("An author") == 3
+    assert "basic test" in result.stdout
+    assert "add line" in result.stdout
+    assert "remove line" in result.stdout
     assert result.exit_code == 0, result.stdout
