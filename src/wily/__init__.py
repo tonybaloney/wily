4e87114c5514433a8a01d0db1c7ddf26029628c3
Anthony Shaw <anthony.p.shaw@gmail.com>
2019-12-06 14:18:10 +1100

    Update pip in tox stage Use the correct coverage target

--- src/wily/__init__.py
+++ src/wily/__init__.py
@@ -7,7 +7,7 @@ import colorlog
 import datetime
 
 
-__version__ = "1.14.0"
+__version__ = "1.14.1"
 
 _handler = colorlog.StreamHandler()
 _handler.setFormatter(colorlog.ColoredFormatter("%(log_color)s%(message)s"))
