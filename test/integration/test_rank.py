76ba2d297fc402cd87efc6ad088dd99440a33f0c
Anthony Shaw <anthony.p.shaw@gmail.com>
2019-12-12 14:38:41 +1100

    Improve the tests

--- test/integration/test_rank.py
+++ test/integration/test_rank.py
@@ -1,6 +1,7 @@
 from click.testing import CliRunner
 
 import wily.__main__ as main
+from git import Actor, Repo
 
 
 def test_rank_no_cache(tmpdir):
@@ -24,6 +25,51 @@ def test_rank_directory_default_metric(builddir):
     assert result.exit_code == 0, result.stdout
 
 
+def test_rank_directory_default_metric_no_path(builddir):
+    """ Test the rank feature with no path """
+    runner = CliRunner()
+    result = runner.invoke(main.cli, ["--path", builddir, "rank"])
+    assert result.exit_code == 0, result.stdout
+
+
+def test_rank_directory_default_metric_master(builddir):
+    """ Test the rank feature with a specific revision. """
+    runner = CliRunner()
+    result = runner.invoke(main.cli, ["--path", builddir, "rank", "-r", "master"])
+    assert result.exit_code == 0, result.stdout
+
+
+def test_rank_directory_default_invalid_revision(builddir):
+    """ Test the rank feature with an invalid revision. """
+    runner = CliRunner()
+    result = runner.invoke(main.cli, ["--path", builddir, "rank", "-r", "xyz"])
+    assert result.exit_code == 1, result.stdout
+
+
+def test_rank_directory_default_unindexed_revision(builddir):
+    """ Test the rank feature with an unindexed revision. """
+    repo = Repo(builddir)
+    with open(builddir / 'test.py', "w") as test_txt:
+        test_txt.write("import abc")
+
+    index = repo.index
+    index.add([str(builddir / 'test.py')])
+
+    author = Actor("An author", "author@example.com")
+    committer = Actor("A committer", "committer@example.com")
+
+    commit = index.commit(
+        "unindexed commit",
+        author=author,
+        committer=committer,
+        author_date="Thu, 28 Apr 2019 22:13:13 +0200",
+        commit_date="Thu, 28 Apr 2019 22:13:13 +0200",
+    )
+    runner = CliRunner()
+    result = runner.invoke(main.cli, ["--path", builddir, "rank", "-r", commit.hexsha])
+    assert result.exit_code == 1, result.stdout
+
+
 def test_rank_single_file_informational(builddir):
     """ Test the rank feature with Informational metric """
     runner = CliRunner()
@@ -42,6 +88,15 @@ def test_rank_directory_custom_metric(builddir):
     assert result.exit_code == 0, result.stdout
 
 
+def test_rank_directory_no_path_target(builddir):
+    """ Test the rank feature with no path target """
+    runner = CliRunner()
+    result = runner.invoke(
+        main.cli, ["rank", "src/", "raw.comments"]
+    )
+    assert result.exit_code == 0, result.stdout
+
+
 def test_rank_directory_limit(builddir):
     """ Test the rank feature with limit """
     runner = CliRunner()
@@ -56,6 +111,15 @@ def test_rank_directory_desc(builddir):
     runner = CliRunner()
     result = runner.invoke(
         main.cli, ["--path", builddir, "rank", "src/", "raw.comments", "--desc"]
+    )
+    assert result.exit_code == 0, result.stdout
+
+
+def test_rank_directory_invalid_key(builddir):
+    """ Test the rank feature descending order with an invalid key """
+    runner = CliRunner()
+    result = runner.invoke(
+        main.cli, ["--path", builddir, "rank", "invalid/", "raw.comments", "--desc"]
     )
     assert result.exit_code == 0, result.stdout
 
