b9f6cbdad214e8efc6e235d60023bb9c8b59db8a
devdanzin <74280297+devdanzin@users.noreply.github.com>
2023-07-09 11:14:00 -0300

    Move expected data out of test functions so black doesn't create ugly code.

--- test/unit/test_helper.py
+++ test/unit/test_helper.py
@@ -10,6 +10,46 @@ DATA = [
     ("data2_1", "data2_2", "data2_3"),
 ]
 
+TABLE_DATA = """
+╒═══════════╤═══════════╤═══════════╕
+│ header1   │ header2   │ header3   │
+╞═══════════╪═══════════╪═══════════╡
+│ data1_1   │ data1_2   │ data1_3   │
+├───────────┼───────────┼───────────┤
+│ data2_1   │ data2_2   │ data2_3   │
+╘═══════════╧═══════════╧═══════════╛
+"""[1:]
+
+JSON_DATA = """[
+  {
+    "header1": "data1_1",
+    "header2": "data1_2",
+    "header3": "data1_3"
+  },
+  {
+    "header1": "data2_1",
+    "header2": "data2_2",
+    "header3": "data2_3"
+  }
+]
+"""
+
+JSON_DATA_WITH_PATH = """[
+  {
+    "header1": "data1_1",
+    "header2": "data1_2",
+    "header3": "data1_3",
+    "Filename": "some_path"
+  },
+  {
+    "header1": "data2_1",
+    "header2": "data2_2",
+    "header3": "data2_3",
+    "Filename": "some_path"
+  }
+]
+"""
+
 
 def test_print_result_empty_json():
     stdout = StringIO()
@@ -29,83 +69,25 @@ def test_print_result_data_json():
     stdout = StringIO()
     with mock.patch("sys.stdout", stdout):
         print_result(True, DATA, HEADERS, DEFAULT_GRID_STYLE)
-    assert (
-        stdout.getvalue()
-        == """[
-  {
-    "header1": "data1_1",
-    "header2": "data1_2",
-    "header3": "data1_3"
-  },
-  {
-    "header1": "data2_1",
-    "header2": "data2_2",
-    "header3": "data2_3"
-  }
-]
-"""
-    )
+    assert stdout.getvalue() == JSON_DATA
 
 
 def test_print_result_data_table():
     stdout = StringIO()
     with mock.patch("sys.stdout", stdout):
         print_result(False, DATA, HEADERS, DEFAULT_GRID_STYLE)
-    assert (
-        stdout.getvalue()
-        == """
-╒═══════════╤═══════════╤═══════════╕
-│ header1   │ header2   │ header3   │
-╞═══════════╪═══════════╪═══════════╡
-│ data1_1   │ data1_2   │ data1_3   │
-├───────────┼───────────┼───────────┤
-│ data2_1   │ data2_2   │ data2_3   │
-╘═══════════╧═══════════╧═══════════╛
-"""[
-            1:
-        ]
-    )
+    assert stdout.getvalue() == TABLE_DATA
 
 
 def test_print_result_data_json_path():
     stdout = StringIO()
     with mock.patch("sys.stdout", stdout):
         print_result(True, DATA, HEADERS, DEFAULT_GRID_STYLE, "some_path")
-    assert (
-        stdout.getvalue()
-        == """[
-  {
-    "header1": "data1_1",
-    "header2": "data1_2",
-    "header3": "data1_3",
-    "Filename": "some_path"
-  },
-  {
-    "header1": "data2_1",
-    "header2": "data2_2",
-    "header3": "data2_3",
-    "Filename": "some_path"
-  }
-]
-"""
-    )
+    assert stdout.getvalue() == JSON_DATA_WITH_PATH
 
 
 def test_print_result_data_table_path():
     stdout = StringIO()
     with mock.patch("sys.stdout", stdout):
         print_result(False, DATA, HEADERS, DEFAULT_GRID_STYLE, "some_path")
-    assert (
-        stdout.getvalue()
-        == """
-╒═══════════╤═══════════╤═══════════╕
-│ header1   │ header2   │ header3   │
-╞═══════════╪═══════════╪═══════════╡
-│ data1_1   │ data1_2   │ data1_3   │
-├───────────┼───────────┼───────────┤
-│ data2_1   │ data2_2   │ data2_3   │
-╘═══════════╧═══════════╧═══════════╛
-"""[
-            1:
-        ]
-    )
+    assert stdout.getvalue() == TABLE_DATA
