0ef60c77b9bef299d64c6d573648a59e471f226e
devdanzin <74280297+devdanzin@users.noreply.github.com>
2023-07-12 13:11:44 -0300

    Add unit tests for the index command.

---
+++ test/unit/test_index_unit.py
@@ -0,0 +1,118 @@
+"""Unit tests for the index command."""
+from io import StringIO
+from unittest import mock
+
+from wily.commands.index import index
+
+EXPECTED = """
+╒════════════╤══════════╤════════════╕
+│ Revision   │ Author   │ Date       │
+╞════════════╪══════════╪════════════╡
+│ abcdef0    │ Author 0 │ 1969-12-31 │
+├────────────┼──────────┼────────────┤
+│ abcdef1    │ Author 1 │ 1969-12-31 │
+├────────────┼──────────┼────────────┤
+│ abcdef2    │ Author 2 │ 1969-12-31 │
+╘════════════╧══════════╧════════════╛
+"""
+EXPECTED = EXPECTED[1:]
+
+EXPECTED_WITH_MESSAGE = """
+╒════════════╤══════════╤═══════════╤════════════╕
+│ Revision   │ Author   │ Message   │ Date       │
+╞════════════╪══════════╪═══════════╪════════════╡
+│ abcdef0    │ Author 0 │ Message 0 │ 1969-12-31 │
+├────────────┼──────────┼───────────┼────────────┤
+│ abcdef1    │ Author 1 │ Message 1 │ 1969-12-31 │
+├────────────┼──────────┼───────────┼────────────┤
+│ abcdef2    │ Author 2 │ Message 2 │ 1969-12-31 │
+╘════════════╧══════════╧═══════════╧════════════╛
+"""
+EXPECTED_WITH_MESSAGE = EXPECTED_WITH_MESSAGE[1:]
+
+EXPECTED_EMPTY = """
+╒════════════╤══════════╤════════╕
+│ Revision   │ Author   │ Date   │
+╞════════════╪══════════╪════════╡
+╘════════════╧══════════╧════════╛
+"""
+EXPECTED_EMPTY = EXPECTED_EMPTY[1:]
+
+EXPECTED_EMPTY_WITH_MESSAGE = """
+╒════════════╤══════════╤═══════════╤════════╕
+│ Revision   │ Author   │ Message   │ Date   │
+╞════════════╪══════════╪═══════════╪════════╡
+╘════════════╧══════════╧═══════════╧════════╛
+"""
+EXPECTED_EMPTY_WITH_MESSAGE = EXPECTED_EMPTY_WITH_MESSAGE[1:]
+
+
+def get_mock_State_and_config(revs):
+    revisions = []
+    for rev in range(revs):
+        rev_dict = {
+            "revision.key": f"abcdef{rev}",
+            "revision.author_name": f"Author {rev}",
+            "revision.message": f"Message {rev}",
+            "revision.date": rev,
+        }
+        mock_revision = mock.Mock(**rev_dict)
+        revisions.append(mock_revision)
+    mock_revisions = mock.Mock(revisions=revisions)
+    mock_state = mock.Mock(index={"git": mock_revisions}, archivers=("git",))
+    mock.seal(mock_state)
+    mock_State = mock.Mock(return_value=mock_state)
+    mock_config = mock.Mock(path="", archiver="", operator="")
+    return mock_State, mock_config
+
+
+def test_index_without_message():
+    mock_State, mock_config = get_mock_State_and_config(3)
+    stdout = StringIO()
+
+    with mock.patch("sys.stdout", stdout), mock.patch(
+        "wily.commands.index.State", mock_State
+    ):
+        index(mock_config, include_message=False)
+
+    assert stdout.getvalue() == EXPECTED
+    mock_State.assert_called_once_with(config=mock_config)
+
+
+def test_index_with_message():
+    mock_State, mock_config = get_mock_State_and_config(3)
+    stdout = StringIO()
+
+    with mock.patch("sys.stdout", stdout), mock.patch(
+        "wily.commands.index.State", mock_State
+    ):
+        index(mock_config, include_message=True)
+
+    assert stdout.getvalue() == EXPECTED_WITH_MESSAGE
+    mock_State.assert_called_once_with(config=mock_config)
+
+
+def test_empty_index_without_message():
+    mock_State, mock_config = get_mock_State_and_config(0)
+    stdout = StringIO()
+
+    with mock.patch("sys.stdout", stdout), mock.patch(
+        "wily.commands.index.State", mock_State
+    ):
+        index(mock_config, include_message=False)
+
+    assert stdout.getvalue() == EXPECTED_EMPTY
+    mock_State.assert_called_once_with(config=mock_config)
+
+
+def test_empty_index_with_message():
+    mock_State, mock_config = get_mock_State_and_config(0)
+    stdout = StringIO()
+
+    with mock.patch("sys.stdout", stdout), mock.patch(
+        "wily.commands.index.State", mock_State
+    ):
+        index(mock_config, include_message=True)
+
+    assert stdout.getvalue() == EXPECTED_EMPTY_WITH_MESSAGE
+    mock_State.assert_called_once_with(config=mock_config)
