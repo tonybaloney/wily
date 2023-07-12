e02a549510d284ae72c9ab35c54e1331f0b5cd73
devdanzin <74280297+devdanzin@users.noreply.github.com>
2023-07-12 18:26:49 -0300

    Add a unit test for HTML report generation.

--- test/unit/test_report_unit.py
+++ test/unit/test_report_unit.py
@@ -348,3 +348,79 @@ def test_report_with_keyerror_changes_only():
         )
     assert stdout.getvalue() == EXPECTED_WITH_KEYERROR_CHANGES_ONLY
     mock_State.assert_called_once_with(mock_config)
+
+
+EXPECTED_HTML = """<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>wily report</title>
+    <link rel="stylesheet" href="css/main.css" type="text/css">
+</head>
+<body class="indexfile">
+    <div class="limiter">
+		<div class="container-table100">
+			<div class="wrap-table100">
+                <table class="index">
+                    <thead>
+                        <tr><th>Revision</th><th>Author</th><th>Date</th><th>Lines of Code</th></tr>
+                    </thead>
+                    <tbody>
+"""
+EXPECTED_HTML += (
+    "                        <tr><td>abcdef0</td><td>Author 0</td><td>1969-12-31</td><td>0 (<span class='orange-color'>-1</span>)</td></tr>"
+    "<tr><td>abcdef1</td><td>Author 1</td><td>1969-12-31</td><td>1 (<span class='orange-color'>-1</span>)</td></tr>"
+    "<tr><td>abcdef2</td><td>Author 2</td><td>1969-12-31</td><td>2 (<span class='orange-color'>-1</span>)</td></tr>"
+    "<tr><td>abcdeff</td><td>Author Someone</td><td>1969-12-31</td><td>3 (<span class='orange-color'>-1</span>)</td></tr>"
+    "<tr><td>abcdeff</td><td>Author Someone</td><td>1969-12-31</td><td>4 (<span class='orange-color'>+1</span>)</td></tr>"
+    "<tr><td>abcdeff</td><td>Author Someone</td><td>1969-12-31</td><td>3 (0)</td></tr>"
+    "<tr><td>abcdeff</td><td>Author Someone</td><td>1969-12-31</td><td>Not found 'some_path.py'</td></tr>"
+)
+EXPECTED_HTML += """
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+</html>"""
+
+
+def test_report_html():
+    path = "test.py"
+    metrics = ("raw.loc",)
+    format = "HTML"
+
+    mock_output, output = get_outputs()
+
+    mock_State, mock_config = get_mock_State_and_config(3, with_keyerror=True)
+    stdout = StringIO()
+
+    with mock.patch("sys.stdout", stdout), mock.patch(
+        "wily.commands.report.State", mock_State
+    ), mock.patch("wily.commands.report.copytree"):
+        report(
+            config=mock_config,
+            path=path,
+            metrics=metrics,
+            n=10,
+            output=mock_output,
+            include_message=False,
+            format=ReportFormat[format],
+            console_format=DEFAULT_GRID_STYLE,
+            changes_only=False,
+        )
+
+    assert output.getvalue() == EXPECTED_HTML
+    mock_State.assert_called_once_with(mock_config)
+
+
+def get_outputs():
+    output = StringIO()
+    enter = mock.MagicMock(return_value=output)
+    writer = mock.MagicMock(__enter__=enter)
+    opener = mock.MagicMock(return_value=writer)
+    mock_output = mock.MagicMock(
+        is_file=True, suffix=".html", parents=[mock.MagicMock()], open=opener
+    )
+    return mock_output, output
