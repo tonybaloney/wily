fa98a4e42fefdab9efcb1fe1e97c8298d795261c
devdanzin <74280297+devdanzin@users.noreply.github.com>
2023-07-13 02:18:05 -0300

    Add more rank unit tests.

--- test/unit/test_rank_unit.py
+++ test/unit/test_rank_unit.py
@@ -48,6 +48,128 @@ def test_rank():
     mock_resolve.assert_called_once()
 
 
+def test_rank_descending():
+    metric = "raw.loc"
+    revision_id = "abcdeff"
+    mock_State, mock_config = get_mock_State_and_config(3)
+    mock_revision = mock.MagicMock(key="abcdeff123123", message="Nothing.")
+    mock_resolve = mock.MagicMock()
+    mock_resolve.cls.find = mock.Mock(return_value=mock_revision)
+    stdout = StringIO()
+
+    with mock.patch("sys.stdout", stdout), mock.patch(
+        "wily.commands.rank.State", mock_State
+    ), mock.patch("wily.commands.rank.resolve_archiver", mock_resolve):
+        rank(
+            config=mock_config,
+            path=None,
+            metric=metric,
+            revision_index=revision_id,
+            limit=None,
+            threshold=None,
+            descending=True,
+        )
+
+    assert stdout.getvalue() == EXPECTED
+    mock_State.assert_called_once_with(mock_config)
+    mock_resolve.assert_called_once()
+
+
+EXPECTED_LIMIT = """
+╒════════╤═════════════════╕
+│ File   │   Lines of Code │
+╞════════╪═════════════════╡
+│ file1  │               3 │
+├────────┼─────────────────┤
+│ Total  │               3 │
+╘════════╧═════════════════╛
+"""
+EXPECTED_LIMIT = EXPECTED_LIMIT[1:]
+
+
+def test_rank_limit():
+    metric = "raw.loc"
+    revision_id = "abcdeff"
+    mock_State, mock_config = get_mock_State_and_config(3)
+    mock_revision = mock.MagicMock(key="abcdeff123123", message="Nothing.")
+    mock_resolve = mock.MagicMock()
+    mock_resolve.cls.find = mock.Mock(return_value=mock_revision)
+    stdout = StringIO()
+
+    with mock.patch("sys.stdout", stdout), mock.patch(
+        "wily.commands.rank.State", mock_State
+    ), mock.patch("wily.commands.rank.resolve_archiver", mock_resolve):
+        rank(
+            config=mock_config,
+            path=None,
+            metric=metric,
+            revision_index=revision_id,
+            limit=1,
+            threshold=None,
+            descending=False,
+        )
+
+    assert stdout.getvalue() == EXPECTED_LIMIT
+    mock_State.assert_called_once_with(mock_config)
+    mock_resolve.assert_called_once()
+
+
+def test_rank_path():
+    metric = "raw.loc"
+    revision_id = "abcdeff"
+    mock_State, mock_config = get_mock_State_and_config(3)
+    mock_revision = mock.MagicMock(key="abcdeff123123", message="Nothing.")
+    mock_resolve = mock.MagicMock()
+    mock_resolve.cls.find = mock.Mock(return_value=mock_revision)
+    stdout = StringIO()
+
+    with mock.patch("sys.stdout", stdout), mock.patch(
+        "wily.commands.rank.State", mock_State
+    ), mock.patch("wily.commands.rank.resolve_archiver", mock_resolve):
+        rank(
+            config=mock_config,
+            path="directory1s",
+            metric=metric,
+            revision_index=revision_id,
+            limit=None,
+            threshold=None,
+            descending=False,
+        )
+
+    assert stdout.getvalue() == ""
+    mock_State.assert_called_once_with(mock_config)
+    mock_resolve.assert_called_once()
+
+
+def test_rank_path_output():
+    metric = "raw.loc"
+    revision_id = "abcdeff"
+    mock_State, mock_config = get_mock_State_and_config(3)
+    mock_revision = mock.MagicMock(key="abcdeff123123", message="Nothing.")
+    mock_resolve = mock.MagicMock()
+    mock_resolve.cls.find = mock.Mock(return_value=mock_revision)
+    mock_iterfilenames = mock.Mock(return_value=("file1", "file2"))
+    stdout = StringIO()
+
+    with mock.patch("sys.stdout", stdout), mock.patch(
+        "wily.commands.rank.State", mock_State
+    ), mock.patch("wily.commands.rank.resolve_archiver", mock_resolve
+    ), mock.patch("radon.cli.harvest.iter_filenames", mock_iterfilenames):
+        rank(
+            config=mock_config,
+            path="directory1s",
+            metric=metric,
+            revision_index=revision_id,
+            limit=None,
+            threshold=None,
+            descending=False,
+        )
+
+    assert stdout.getvalue() == EXPECTED
+    mock_State.assert_called_once_with(mock_config)
+    mock_resolve.assert_called_once()
+
+
 def test_keyerror():
     metric = "raw.loc"
     revision_id = "abcdeff"
