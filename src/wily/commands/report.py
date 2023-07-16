030b1129983b869a47384567343c79d5f56e537a
devdanzin <74280297+devdanzin@users.noreply.github.com>
2023-07-15 22:06:53 -0300

    Increase padding to avoid the table being wider than the terminal.

--- src/wily/commands/report.py
+++ src/wily/commands/report.py
@@ -225,12 +225,12 @@ def get_maxcolwidth(headers):
     width = shutil.get_terminal_size()[0]
     columns = len(headers)
     if width > 125:
-        padding = 0
+        padding = 2
     elif width > 95:
         padding = 3
     else:
         padding = 5
-    maxcolwidth = width // columns - padding
+    maxcolwidth = width // columns - padding - 1
     if not width % columns:
-        maxcolwidth -= 1
-    return maxcolwidth
+        maxcolwidth -= 2
+    return max(maxcolwidth, 1)
