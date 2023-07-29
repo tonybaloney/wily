eefe7a9465f2bddf998251360b8226c161c29073
devdanzin <74280297+devdanzin@users.noreply.github.com>
2023-07-29 20:14:24 -0300

    Add unit tests for index() with wrapping.

--- test/unit/test_index_unit.py
+++ test/unit/test_index_unit.py
@@ -51,6 +51,39 @@ def test_index_without_message(capsys):
     mock_State.assert_called_once_with(config=mock_config)
 
 
+EXPECTED_WRAPPED = f"""
+╒═════════╤═════════╤════════╕
+│ Revis   │ Autho   │ Date   │
+│ ion     │ r       │        │
+╞═════════╪═════════╪════════╡
+│ abcde   │ Autho   │ {fd(0)[:5]}  │
+│ f0      │ r 0     │ {fd(0)[5:]}  │
+├─────────┼─────────┼────────┤
+│ abcde   │ Autho   │ {fd(1)[:5]}  │
+│ f1      │ r 1     │ {fd(1)[5:]}  │
+├─────────┼─────────┼────────┤
+│ abcde   │ Autho   │ {fd(2)[:5]}  │
+│ f2      │ r 2     │ {fd(2)[5:]}  │
+╘═════════╧═════════╧════════╛
+"""
+EXPECTED_WRAPPED = EXPECTED_WRAPPED[1:]
+
+
+def test_index_without_message_wrapped(capsys):
+    mock_State, mock_config = get_mock_State_and_config(3)
+    mock_get_terminal_size = mock.Mock(return_value=(30, 24))
+    mock_shutil = mock.Mock(get_terminal_size=mock_get_terminal_size)
+
+    with mock.patch("wily.helper.shutil", mock_shutil), mock.patch(
+        "wily.commands.index.State", mock_State
+    ):
+        index(mock_config, include_message=False, wrap=True)
+
+    captured = capsys.readouterr()
+    assert captured.out == EXPECTED_WRAPPED
+    mock_State.assert_called_once_with(config=mock_config)
+
+
 EXPECTED_WITH_MESSAGE = f"""
 ╒════════════╤══════════╤═══════════╤════════════╕
 │ Revision   │ Author   │ Message   │ Date       │
@@ -73,6 +106,40 @@ def test_index_with_message(capsys):
 
     captured = capsys.readouterr()
     assert captured.out == EXPECTED_WITH_MESSAGE
+    mock_State.assert_called_once_with(config=mock_config)
+
+
+EXPECTED_WITH_MESSAGE_WRAPPED = f"""
+╒═════════╤═════════╤═════════╤════════╕
+│ Revis   │ Autho   │ Messa   │ Date   │
+│ ion     │ r       │ ge      │        │
+╞═════════╪═════════╪═════════╪════════╡
+│ abcde   │ Autho   │ Messa   │ {fd(0)[:5]}  │
+│ f0      │ r 0     │ ge 0    │ {fd(0)[5:]}  │
+├─────────┼─────────┼─────────┼────────┤
+│ abcde   │ Autho   │ Messa   │ {fd(1)[:5]}  │
+│ f1      │ r 1     │ ge 1    │ {fd(1)[5:]}  │
+├─────────┼─────────┼─────────┼────────┤
+│ abcde   │ Autho   │ Messa   │ {fd(2)[:5]}  │
+│ f2      │ r 2     │ ge 2    │ {fd(2)[5:]}  │
+╘═════════╧═════════╧═════════╧════════╛
+"""
+EXPECTED_WITH_MESSAGE_WRAPPED = EXPECTED_WITH_MESSAGE_WRAPPED[1:]
+
+
+def test_index_with_message_wrapped(capsys):
+    mock_State, mock_config = get_mock_State_and_config(3)
+
+    mock_get_terminal_size = mock.Mock(return_value=(45, 24))
+    mock_shutil = mock.Mock(get_terminal_size=mock_get_terminal_size)
+
+    with mock.patch("wily.helper.shutil", mock_shutil), mock.patch(
+        "wily.commands.index.State", mock_State
+    ):
+        index(mock_config, include_message=True, wrap=True)
+
+    captured = capsys.readouterr()
+    assert captured.out == EXPECTED_WITH_MESSAGE_WRAPPED
     mock_State.assert_called_once_with(config=mock_config)
 
 
