db52493dd4055d7b63851891eba1f88131d6a136
devdanzin <74280297+devdanzin@users.noreply.github.com>
2023-07-29 20:43:15 -0300

    Add unit test for list_metrics with wrapping.

--- test/unit/test_list_metrics_unit.py
+++ test/unit/test_list_metrics_unit.py
@@ -1,6 +1,5 @@
 """Unit tests for the list_metrics command."""
 
-from io import StringIO
 from statistics import mean
 from unittest import mock
 
@@ -73,3 +72,96 @@ def test_list_metrics(capsys):
     list_metrics(wrap=False)
     captured = capsys.readouterr()
     assert captured.out == EXPECTED
+
+
+EXPECTED_WRAPPED = f"""
+cyclomatic operator:
+╒════════════╤═══════════════╤══════════╤════════════════╤════════════════╕
+│ Name       │ Description   │ Type     │ Measure        │ Aggregate      │
+╞════════════╪═══════════════╪══════════╪════════════════╪════════════════╡
+│ complexity │ Cyclomatic    │ <class   │ MetricType.Aim │ {str(mean)[:15]}│
+│            │ Complexity    │ 'float'> │ Low            │ {str(mean)[15:29]} │
+│            │               │          │                │ {str(mean)[29:]:<15}│
+╘════════════╧═══════════════╧══════════╧════════════════╧════════════════╛
+maintainability operator:
+╒════════╤════════════════╤═══════════════╤════════════════╤════════════════╕
+│ Name   │ Description    │ Type          │ Measure        │ Aggregate      │
+╞════════╪════════════════╪═══════════════╪════════════════╪════════════════╡
+│ rank   │ Maintainabilit │ <class 'str'> │ MetricType.Inf │ {str(mode)[:15]}│
+│        │ y Ranking      │               │ ormational     │ {str(mode)[15:29]} │
+│        │                │               │                │ {str(mode)[29:]:<15}│
+├────────┼────────────────┼───────────────┼────────────────┼────────────────┤
+│ mi     │ Maintainabilit │ <class        │ MetricType.Aim │ {str(mean)[:15]}│
+│        │ y Index        │ 'float'>      │ High           │ {str(mean)[15:29]} │
+│        │                │               │                │ {str(mean)[29:]:<15}│
+╘════════╧════════════════╧═══════════════╧════════════════╧════════════════╛
+raw operator:
+╒════════════════╤════════════════╤═══════════════╤════════════════╤═══════════════╕
+│ Name           │ Description    │ Type          │ Measure        │ Aggregate     │
+╞════════════════╪════════════════╪═══════════════╪════════════════╪═══════════════╡
+│ loc            │ Lines of Code  │ <class 'int'> │ MetricType.Inf │ <built-in     │
+│                │                │               │ ormational     │ function sum> │
+├────────────────┼────────────────┼───────────────┼────────────────┼───────────────┤
+│ lloc           │ L Lines of     │ <class 'int'> │ MetricType.Aim │ <built-in     │
+│                │ Code           │               │ Low            │ function sum> │
+├────────────────┼────────────────┼───────────────┼────────────────┼───────────────┤
+│ sloc           │ S Lines of     │ <class 'int'> │ MetricType.Aim │ <built-in     │
+│                │ Code           │               │ Low            │ function sum> │
+├────────────────┼────────────────┼───────────────┼────────────────┼───────────────┤
+│ comments       │ Multi-line     │ <class 'int'> │ MetricType.Aim │ <built-in     │
+│                │ comments       │               │ High           │ function sum> │
+├────────────────┼────────────────┼───────────────┼────────────────┼───────────────┤
+│ multi          │ Multi lines    │ <class 'int'> │ MetricType.Inf │ <built-in     │
+│                │                │               │ ormational     │ function sum> │
+├────────────────┼────────────────┼───────────────┼────────────────┼───────────────┤
+│ blank          │ blank lines    │ <class 'int'> │ MetricType.Inf │ <built-in     │
+│                │                │               │ ormational     │ function sum> │
+├────────────────┼────────────────┼───────────────┼────────────────┼───────────────┤
+│ single_comment │ Single comment │ <class 'int'> │ MetricType.Inf │ <built-in     │
+│ s              │ lines          │               │ ormational     │ function sum> │
+╘════════════════╧════════════════╧═══════════════╧════════════════╧═══════════════╛
+halstead operator:
+╒════════════╤════════════════╤═══════════════╤════════════════╤═══════════════╕
+│ Name       │ Description    │ Type          │ Measure        │ Aggregate     │
+╞════════════╪════════════════╪═══════════════╪════════════════╪═══════════════╡
+│ h1         │ Unique         │ <class 'int'> │ MetricType.Aim │ <built-in     │
+│            │ Operands       │               │ Low            │ function sum> │
+├────────────┼────────────────┼───────────────┼────────────────┼───────────────┤
+│ h2         │ Unique         │ <class 'int'> │ MetricType.Aim │ <built-in     │
+│            │ Operators      │               │ Low            │ function sum> │
+├────────────┼────────────────┼───────────────┼────────────────┼───────────────┤
+│ N1         │ Number of      │ <class 'int'> │ MetricType.Aim │ <built-in     │
+│            │ Operands       │               │ Low            │ function sum> │
+├────────────┼────────────────┼───────────────┼────────────────┼───────────────┤
+│ N2         │ Number of      │ <class 'int'> │ MetricType.Aim │ <built-in     │
+│            │ Operators      │               │ Low            │ function sum> │
+├────────────┼────────────────┼───────────────┼────────────────┼───────────────┤
+│ vocabulary │ Unique         │ <class 'int'> │ MetricType.Aim │ <built-in     │
+│            │ vocabulary (h1 │               │ Low            │ function sum> │
+│            │ + h2)          │               │                │               │
+├────────────┼────────────────┼───────────────┼────────────────┼───────────────┤
+│ length     │ Length of      │ <class 'int'> │ MetricType.Aim │ <built-in     │
+│            │ application    │               │ Low            │ function sum> │
+├────────────┼────────────────┼───────────────┼────────────────┼───────────────┤
+│ volume     │ Code volume    │ <class        │ MetricType.Aim │ <built-in     │
+│            │                │ 'float'>      │ Low            │ function sum> │
+├────────────┼────────────────┼───────────────┼────────────────┼───────────────┤
+│ difficulty │ Difficulty     │ <class        │ MetricType.Aim │ <built-in     │
+│            │                │ 'float'>      │ Low            │ function sum> │
+├────────────┼────────────────┼───────────────┼────────────────┼───────────────┤
+│ effort     │ Effort         │ <class        │ MetricType.Aim │ <built-in     │
+│            │                │ 'float'>      │ Low            │ function sum> │
+╘════════════╧════════════════╧═══════════════╧════════════════╧═══════════════╛
+"""
+EXPECTED_WRAPPED = EXPECTED_WRAPPED[1:]
+
+
+def test_list_metrics_wrapped(capsys):
+    mock_get_terminal_size = mock.Mock(return_value=(85, 24))
+    mock_shutil = mock.Mock(get_terminal_size=mock_get_terminal_size)
+
+    with mock.patch("wily.helper.shutil", mock_shutil):
+        list_metrics(wrap=True)
+    captured = capsys.readouterr()
+    print(captured.out)
+    assert captured.out == EXPECTED_WRAPPED
