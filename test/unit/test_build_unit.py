166110f14c514be2edd6f8d898f92f6238b1ca8c
devdanzin <74280297+devdanzin@users.noreply.github.com>
2023-08-27 14:30:56 -0300

    Patch resolve_operator().

--- test/unit/test_build_unit.py
+++ test/unit/test_build_unit.py
@@ -72,7 +72,9 @@ def config():
 
 def test_build_simple(config):
     _test_operators = (MockOperator,)
-    with patch("wily.state.resolve_archiver", return_value=MockArchiver):
+    with patch("wily.state.resolve_archiver", return_value=MockArchiver), patch(
+        "wily.operators.resolve_operator", return_value=MockOperatorCls
+    ):
         result = build.build(config, MockArchiver, _test_operators)
     assert result is None
 
