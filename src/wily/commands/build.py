54f6268d50950b03cb7f9b5337ec008d8d2931ca
devdanzin <74280297+devdanzin@users.noreply.github.com>
2023-10-09 09:02:40 -0300

    Check against config.targets being ['.'] in build().

--- src/wily/commands/build.py
+++ src/wily/commands/build.py
@@ -103,7 +103,7 @@ def build(config: WilyConfig, archiver: Archiver, operators: List[Operator]) -> 
                 targets = [
                     str(pathlib.Path(config.path) / pathlib.Path(file))
                     for file in revision.added_files + revision.modified_files
-                    if not config.targets
+                    if config.targets == ["."]
                     or any(
                         # Check that changed files are children of the targets
                         True
