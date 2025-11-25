e52db29c1d2d3966dbd4335527be574c0b59b880
Anthony Shaw <anthony.p.shaw@gmail.com>
2025-11-25 16:45:43 +1100

    Simplify rust module

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,17 +2,8 @@ use pyo3::prelude::*;
 
 mod raw;
 
-/// Example function implemented in Rust to demonstrate PyO3 integration.
-#[pyfunction]
-fn rust_add(a: i64, b: i64) -> PyResult<i64> {
-    Ok(a + b)
-}
-
-/// PyO3 module definition exposed to Python as `wily._rust`.
 #[pymodule]
 fn _rust(_py: Python<'_>, module: &Bound<'_, PyModule>) -> PyResult<()> {
-    module.add_function(wrap_pyfunction!(rust_add, module)?)?;
     raw::register(module)?;
-    module.add("__version__", env!("CARGO_PKG_VERSION"))?;
     Ok(())
 }
